f5c091621ca294fa3f0e6c5890128d71969f0e33
Paul Swartz <paul@paulswartz.net>
2022-09-07 12:24:56 -0400

    fix: use correct .zip file name for Elixir 1.14

--- build_elixir_container.ps1
+++ build_elixir_container.ps1
@@ -18,6 +18,13 @@ $erlimage="${ImageNameRoot}erlang"
 $erltag="${ErlangVersion}-windows-${WindowsVersion}"
 $elixirimage="${ImageNameRoot}elixir"
 $elixirtag="${ElixirVersion}-erlang-${erltag}"
+$ElixirZip="Precompiled.zip"
+
+if ([System.Version]$ElixirVersion -ge [System.Version]"1.14.0") {
+    # With Elixir 1.14.0, the releases are precompiled for the specific Erlang version.
+    $ErlangMajorVersion=$ErlangVersion.split(".")[0]
+    $ElixirZip="elixir-otp-$ErlangMajorVersion"
+}
 
 docker manifest inspect "${elixirimage}:${elixirtag}" | Out-Null
 if (-not $Force -and $?) {
@@ -26,7 +33,8 @@ if (-not $Force -and $?) {
   docker build docker -f docker/Dockerfile.elixir `
     --build-arg BUILD_IMAGE="mcr.microsoft.com/windows/servercore:${WindowsVersion}" `
     --build-arg FROM_IMAGE="${erlimage}:${erltag}" `
-    --build-arg ELIXIR_VERSION=$ElixirVersion `
+    --build-arg ELIXIR_VERSION="$ElixirVersion" `
+    --build-arg ELIXIR_ZIP="$ElixirZip" `
     --tag "${elixirimage}:${elixirtag}"
 
   docker push "${elixirimage}:${elixirtag}"
