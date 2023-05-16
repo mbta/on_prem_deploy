cb78cbde8a8f95e54b0e437c91580a36c968d493
Paul Swartz <paul@paulswartz.net>
2023-05-16 16:44:43 -0400

    refactor: pull CloudInit ISO generation into a separate script

---
+++ linux/build_cidata_iso.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Build the CIDATA seed.iso
+# $ bash build_cidata_iso.sh [hostname]
+
+set -e
+
+HOSTNAME=$1
+tmpdir=tmp
+
+mkdir -p $tmpdir/config
+cat > $tmpdir/config/meta-data <<-EOF
+local-hostname: $HOSTNAME
+EOF
+
+cat cloud-init/user-data \
+    | sed "s/GITHUB_REPO/${GITHUB_REPO/\//\\/}/g" \
+    | sed "s/GIT_BRANCH/$GIT_BRANCH/g" \
+    | sed "s/ANSIBLE_VAULT_PASSWORD/$ANSIBLE_VAULT_PASSWORD/g" \
+    > $tmpdir/config/user-data
+
+echo > $tmpdir/config/vendor-data
+
+rm -f $tmpdir/$HOSTNAME.iso
+hdiutil makehybrid -o $tmpdir/$HOSTNAME.iso -hfs -joliet -iso -default-volume-name cidata $tmpdir/config/
+rm -r $tmpdir/config
+
+echo Built $tmpdir/$HOSTNAME.iso
