40bacb25b3f357dc3a45ae8b41e98609c03d5f4a
Paul Swartz <paul@paulswartz.net>
2023-06-07 16:35:58 -0400

    chore: script cleanups

--- linux/roles/onprem/files/google-authenticator.sh
+++ linux/roles/onprem/files/google-authenticator.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env sh
-# only require this for members of the `users` group
-id -nG "$USER" | grep -qw "users" || return
-trap "exit 1" SIGINT SIGQUIT
-if [ ! -f $HOME/.google_authenticator ]; then
+
+# trap exits, rather than allowing a user to ^C out of the script
+trap "exit 1" INT QUIT
+
+google_authenticator_path="${HOME:?}"/.google_authenticator
+
+if [ ! -f "$google_authenticator_path" ]; then
     google-authenticator -ftDW -r 3 -R 30
-fi
-if [ ! -f $HOME/.google_authenticator ]; then
-    exit 1
+
+    # Ensure that the Google Authenticator file was created
+    test -f "$google_authenticator_path" || exit 1
 fi
 
+# Either run the shell, or the original command
 if [ -z "$SSH_ORIGINAL_COMMAND" ]; then
-    exec "$SHELL"
+    exec "$SHELL" -i
 else
     exec "$SHELL" -c "$SSH_ORIGINAL_COMMAND"
 fi
